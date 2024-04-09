b477a6fda905ea7450871a89a478e126446d512c
Lucille Alvar <lucille.alvar@gmail.com>
2024-04-08 19:39:58 -0600

    Added overlay for each selected images - made images bigger when shown in pop up - positioned the exit button on enlargeimage component fixed Changes to be committed: 	modified:   src/Pages/ProjectsPage/ProjectsPage.jsx 	modified:   src/Pages/ProjectsPage/ProjectsPage.scss 	modified:   src/components/EnlargeImage/EnlargeImage.jsx 	modified:   src/components/EnlargeImage/EnlargeImage.scss

--- src/components/EnlargeImage/EnlargeImage.jsx
+++ src/components/EnlargeImage/EnlargeImage.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import './EnlargeImage.scss'
+import "./EnlargeImage.scss";
 
 const EnlargeImage = ({ selectedImage, onClose, selectedOpen }) => {
   return (
     <div className="enlarged">
-        <div className="enlarged-image">
-        <div className="enlarged-image_header">
-            <p onClick={onClose}>&times;</p>
+      <div className="enlarged-image">
+        {selectedImage && (
+          <div className="enlarged-image_content">
+            <img className="img" src={selectedImage} alt="Enlarged" />
+          </div>
+        )}{" "}
+        <div className="enlarged-image_header" onClick={onClose}>
+          <p className="x">&times;</p>
         </div>
-      {selectedImage && (
-        <div className="enlarged-image_content">
-          <img className="img" src={selectedImage} alt="Enlarged" />
-        </div>
-      )}{" "}
-        </div>
+      </div>
     </div>
   );
 };
